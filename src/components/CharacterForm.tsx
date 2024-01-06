14118bf91c1f9234913f3c39ce5c88dfd209446b
MarcosPimienta <fenix3819@gmail.com>
2024-01-06 17:38:16 -0500

    This is the start of a Redux learning project

---
+++ src/components/CharacterForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CharacterForm = () => {
+  return (
+    <div className="character-form">
+      <h1>Dungeons & Redux</h1>
+      {/* Other components will be included here */}
+    </div>
+  );
+};
+
+export default CharacterForm;
